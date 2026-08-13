71de36560ae75d5835ccd9ce6654d26ec935a78e
tiagoquintana10 <tquintana647@alumnos.frh.utn.edu.ar>
2026-08-12 21:24:12 -0300

    main-menu
    incorporamos el diseño del menu de inicio

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 import { MainMenu } from './components/main-menu/main-menu';
+import { ChessBoard } from './components/chess-board/chess-board';
 
 export const routes: Routes = [
 
     {
         path: '',
         component: MainMenu
+    },
+    {
+        path: 'jugar-local',
+        component: ChessBoard
     }
 ];
